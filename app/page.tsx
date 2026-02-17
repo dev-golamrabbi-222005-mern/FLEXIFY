cd7a4bd710939ae49487626de75ba9c3529efcc0
abdullah222005 <g.rabbi2005.555@gmail.com>
2026-02-17 23:13:32 +0600

    [DONE]: Implemented the Success Story Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import FlexifyHero from "@/components/home/FlexifyHero";
 import Pricing from "@/components/home/Pricing";
+import SuccessStory from "@/components/home/SuccessStories";
 
 export default function Home() {
   return (
     <div>
       <FlexifyHero/>
+      <SuccessStory/>
       <Pricing/>
     </div>
   );
