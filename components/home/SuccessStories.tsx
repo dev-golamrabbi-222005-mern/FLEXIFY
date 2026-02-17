10972fe74d59f31a9c8e5fd0b98f8caf23361e72
abdullah222005 <g.rabbi2005.555@gmail.com>
2026-02-17 22:43:24 +0600

    [ADDED]: Success story file created

---
+++ components/home/SuccessStories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SuccessStories = () => {
+    return (
+        <div>
+            It is our success story section
+        </div>
+    );
+};
+
+export default SuccessStories;
