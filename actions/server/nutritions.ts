02f147088bb3a995a64cfc3ef212f88f596dc5a8
Md Ashiqur Rahman Pranto <pranto.gpi@gmail.com>
2026-02-25 12:24:13 +0600

    [MODIFIED]: Nutrition API

--- actions/server/nutritions.ts
+++ actions/server/nutritions.ts
@@ -1,28 +1,13 @@
 "use server";
 
 import { dbConnect } from "@/lib/dbConnect";
-import { ObjectId } from "mongodb";
 
-export interface Meal {
+export interface Nutrition {
   name: string;
   calories: number;
   protein: number;  // grams
   carbs: number;    // grams
   fat: number;      // grams
-}
-
-export interface Nutrition {
-  _id?: ObjectId;
-  userId: ObjectId;     // reference to users collection
-  date: Date;
-
-  meals: Meal[];
-
-  waterGlassCount: number;  // total glasses per day
-  sleepHours: number;       // total sleep duration
-
-  createdAt?: Date;
-  updatedAt?: Date;
 }
 
 export const getNutritions = async (): Promise<Nutrition[]> => {
