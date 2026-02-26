1daf8aa6aa4ba1d1cd05987946ad49baedfd5fee
Md Ashiqur Rahman Pranto <pranto.gpi@gmail.com>
2026-02-26 13:02:38 +0600

    [FIXED]: data loading issue

--- app/(marketing)/coaches/page.tsx
+++ app/(marketing)/coaches/page.tsx
@@ -8,22 +8,22 @@ const CoachesPage = () => {
   const { data: coaches, isLoading, isError } = useQuery({
     queryKey: ["coaches"],
     queryFn: async () => {
-      const { data } = await axios.get("/data.json");
+      const { data } = await axios.get("data.json");
       return data;
     },
   });
 
   if (isLoading) return <div className="min-h-screen flex items-center justify-center font-black uppercase tracking-widest text-[var(--primary)]">Loading Flexify Coaches...</div>;
-  if (isError) return <div className="min-h-screen flex items-center justify-center text-red-500 font-bold">Error loading coaches data.</div>;
+  if (isError) return <div className="flex items-center justify-center min-h-screen font-bold text-red-500">Error loading coaches data.</div>;
 
   return (
-    <div className="min-h-screen py-12 px-6 max-w-7xl mx-auto">
+    <div className="min-h-screen px-6 py-12 mx-auto max-w-7xl">
       
         <h1 className="text-4xl mb-10 md:text-5xl font-black text-[var(--text-primary)] uppercase text-center  tracking-tighter">
           Our Expert <span className="text-[var(--primary)]">Coaches</span>
         </h1>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {coaches?.map((coach: any) =><CoachCard key={coach._id} coach={coach} />)}
       </div>
     </div>
