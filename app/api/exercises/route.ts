ebc388c9bd50e96da1fa24cf2582c26c0bd6876f
abdullah222005 <g.rabbi2005.555@gmail.com>
2026-03-06 14:08:55 +0600

    [MODIFIED] Solved excercise error.

--- app/api/exercises/route.ts
+++ app/api/exercises/route.ts
@@ -1,5 +1,5 @@
 import { dbConnect } from "@/lib/dbConnect";
-import { ObjectId } from "mongodb";
+import { Filter, ObjectId } from "mongodb";
 
 export interface Exercise {
   _id?: ObjectId;
@@ -16,63 +16,72 @@ export interface Exercise {
   images: string[];
 }
 
-export async function GET(request: Request): Promise<Response> {
-    try {
-        const { searchParams } = new URL(request.url);
-        
-        const page = parseInt(searchParams.get("page") || "1");
-        const limit = parseInt(searchParams.get("limit") || "12");
-        const search = searchParams.get("search") || "";
-        const muscle = searchParams.get("muscle") || "";
-        const level = searchParams.get("level") || "";
-        const equipment = searchParams.get("equipment") || "";
-        const category = searchParams.get("category") || "";
-        const force = searchParams.get("force") || "";
+type Context = {
+  params: Promise<{ id: string }>;
+};
 
-        const skip = (page - 1) * limit;
+export async function GET(
+  request: Request,
+  context: Context, 
+): Promise<Response> {
+  try {
+    const { id } = await context.params;
+    const { searchParams } = new URL(request.url);
 
-       
-        let query: any = {};
-        if (search) {
-            query.name = { $regex: search, $options: "i" }; 
-        }
-        if (muscle) {
-            query.primaryMuscles = { $in: [muscle.toLowerCase()] };
-        }
-        if (level) {
-            query.level = level.toLowerCase();
-        }
-        if (equipment) {
-            query.equipment = equipment.toLowerCase();
-        }
-        if (category) {
-            query.category = category.toLowerCase();
-        }
-        if (force) {
-            query.force = force.toLowerCase();
-        }
-        const collection = await dbConnect<Exercise>("exercises");
-        
-        const exercises = await collection
-            .find(query)
-            .skip(skip)
-            .limit(limit)
-            .toArray();
+    const page = parseInt(searchParams.get("page") || "1");
+    const limit = parseInt(searchParams.get("limit") || "12");
+    const search = searchParams.get("search") || "";
+    const muscle = searchParams.get("muscle") || "";
+    const level = searchParams.get("level") || "";
+    const equipment = searchParams.get("equipment") || "";
+    const category = searchParams.get("category") || "";
+    const force = searchParams.get("force") || "";
 
-        const total = await collection.countDocuments(query);
+    const skip = (page - 1) * limit;
 
-        return Response.json({
-            exercises,
-            total,
-            currentPage: page,
-            totalPages: Math.ceil(total / limit)
-        }, { status: 200 });
-
-    } catch (error) {
-        console.error("Exercise Fetch Error:", error);
-        return Response.json(
-            { message: "Failed to fetch exercises" },
-            { status: 500 }
-        );
+    const query: Filter<Exercise> = {};
+    if (search) {
+      query.name = { $regex: search, $options: "i" };
     }
+    if (muscle) {
+      query.primaryMuscles = { $in: [muscle.toLowerCase()] };
+    }
+    if (level) {
+      query.level = level.toLowerCase();
+    }
+    if (equipment) {
+      query.equipment = equipment.toLowerCase();
+    }
+    if (category) {
+      query.category = category.toLowerCase();
+    }
+    if (force) {
+      query.force = force.toLowerCase();
+    }
+    const collection = await dbConnect<Exercise>("exercises");
+
+    const exercises = await collection
+      .find(query)
+      .skip(skip)
+      .limit(limit)
+      .toArray();
+
+    const total = await collection.countDocuments(query);
+
+    return Response.json(
+      {
+        exercises,
+        total,
+        currentPage: page,
+        totalPages: Math.ceil(total / limit),
+      },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error("Exercise Fetch Error:", error);
+    return Response.json(
+      { message: "Failed to fetch exercises" },
+      { status: 500 },
+    );
+  }
 }
