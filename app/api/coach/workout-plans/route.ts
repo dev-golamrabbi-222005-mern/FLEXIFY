39032416113a46b2dd12c8d9fdb23c8c3c6fc9a7
Md Ashiqur Rahman Pranto <pranto.gpi@gmail.com>
2026-03-29 15:32:14 +0600

    [DONE]: implemented workout plan add functionality in coach dashboard's workout page

--- app/api/coach/workout-plans/route.ts
+++ app/api/coach/workout-plans/route.ts
@@ -4,12 +4,30 @@ import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async(req: NextRequest) => {
-    const session = await getServerSession(authOptions);
+    const { searchParams } = new URL(req.url);
+    const coachId = searchParams.get("coachId");
+
+    if (!coachId) {
+        return NextResponse.json({ error: "coachId required" }, { status: 400 });
+    }
     const workout_plans = await dbConnect("workout_plans").find({
-        coach_id: session?.user?.id,
+        coachId,
     }).toArray();
     return NextResponse.json(
         workout_plans,
         {status: 200}
     );
+}
+
+export const POST = async(req: NextRequest) => {
+    const session = await getServerSession(authOptions);
+    if (!session?.user?.email) {
+        return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+    const body = await req.json();
+    const coachSession = dbConnect("workout_plans");
+    const result = await coachSession.insertOne({
+        ...body,
+    });
+    return NextResponse.json(result);
 }
